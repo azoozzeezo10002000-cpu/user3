adf85caba73d6c526b82b6d2a2374632d5c2c347
azoozzeezo10002000-cpu <azoozzeezo10002000@gmail.com>
2026-05-03 14:14:45 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-// sw.js
-const CACHE_NAME = 'user-app-v1';
+const CACHE_NAME = 'user-app-v2';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
@@ -8,7 +7,8 @@ self.addEventListener('install', (event) => {
         './index.html',
         './style.css',
         './script.js',
-        './icon.png'
+        './icon-192.png',
+        './icon-512.png'
       ]);
     })
   );
